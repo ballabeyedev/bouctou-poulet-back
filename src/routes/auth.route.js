9c3b98b51f3d2b5e25074b1e5d093050675945a0
Balla221 <balla.beye_220540@groupeiam.com>
2026-01-19 21:06:47 +0000

    Modif

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const upload = require('../middlewares/upload.middleware');
 const auth = require('../middlewares/auth.middleware');
+const commendeClientController = require('../controllers/client/commende.controller');
 
 router.post('/register', upload.single('photoProfil'), authController.inscriptionUser);
 router.post('/login', authController.login);
+
+
+// Commander un produit
+router.post(
+  '/commander-produit-client',
+  commendeClientController.commenderProduitsClient
+);
 
 module.exports = router;
