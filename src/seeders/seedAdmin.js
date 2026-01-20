4006a3ae1b4d62c3a7f1830599686d9af68adb9a
Balla221 <balla.beye_220540@groupeiam.com>
2026-01-20 21:47:06 +0000

    Modif 7

---
+++ src/seeders/seedAdmin.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.model');
+
+async function seedAdmin() {
+  try {
+    // 🔍 Vérifier si un admin existe déjà
+    const adminExiste = await User.findOne({
+      where: { role: 'Admin' }
+    });
+
+    if (adminExiste) {
+      console.log('ℹ️ Admin déjà existant, seed ignoré');
+      return;
+    }
+
+    // 🔐 Hash du mot de passe
+    const passwordHash = await bcrypt.hash('Toure@223', 10);
+
+    // 👤 Création de l'admin
+    await User.create({
+      nom: 'TOURE',
+      prenom: 'Oumar',
+      email: 'ot218053@gmail.com',
+      mot_de_passe: passwordHash,
+      adresse: 'Mali, Bamako',
+      telephone: '+22391178664',
+      role: 'Admin',
+      statut: 'actif'
+    });
+
+    console.log('✅ Compte ADMIN créé avec succès');
+
+  } catch (error) {
+    console.error('❌ Erreur lors de la création de l’admin :', error);
+  }
+}
+
+module.exports = seedAdmin;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const sequelize = require('./config/db');
 const app = require('./app');
+const seedAdmin = require('./seeders/seedAdmin');
 
 (async () => {
   try {
     await sequelize.sync(); // ⚠️ PAS alter en prod
     console.log('✅ Base de données synchronisée');
 
+    // 🔥 Création auto de l'admin
+    await seedAdmin();
+
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, '0.0.0.0', () => {
       console.log(`🚀 Serveur démarré sur le port ${PORT}`);
     });
+
   } catch (error) {
     console.error('❌ Erreur serveur :', error);
   }
